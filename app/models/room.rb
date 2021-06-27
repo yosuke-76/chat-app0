454ae62fcbb1ae8f140ea80b3c2bc080a89c43f8
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-06-27 23:22:27 +0900

    プルダウンの実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,4 @@
-class RoomUser < ApplicationRecord
-  belongs_to :room
-  belongs_to :user
+class Room < ApplicationRecord
+  has_many :room_users
+  has_many :users, through: :room_users
 end
--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,4 +1,4 @@
-class Room < ApplicationRecord
-  has_many :room_users
-  has_many :users, through: :room_users
+class RoomUser < ApplicationRecord
+  belongs_to :room
+  belongs_to :user
 end
