e490869b34cefa4ada64741375c72da8832d4f9c
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-06-28 15:08:05 +0900

    統合テストの実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 I18n.locale = "en"
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -64,4 +65,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include SignInSupport
 end
